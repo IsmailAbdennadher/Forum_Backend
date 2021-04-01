e7043558df9a516bd3595f4a1447f1d3cf4945ab
Abdennadher Ismail <ismail.abdennadher@aymax.fr>
2021-04-01 11:13:22 +0100

    fixed count of all likes on a post finally

--- src/main/java/com/aymax/forum/repository/LikePostRepository.java
+++ src/main/java/com/aymax/forum/repository/LikePostRepository.java
@@ -17,8 +17,9 @@ public interface LikePostRepository extends JpaRepository<LikePost, Long> {
     @Query(value = "SELECT count(*) FROM Like_Post a WHERE a.post_id = ?1",nativeQuery = true)
     int countPostLikes(long idPost);
 
-    @Query(value = "SELECT count(*) + count(distinct lp.post_id) FROM forum.comment as c left join forum.like_comment as lc on lc.comment_id=c.id \n" +
-            "LEFT join forum.like_post as lp on lp.post_id = c.belong_post_id \n" +
-            "where c.belong_post_id = ?1 and lc.comment_id is not null ;",nativeQuery = true)
+    @Query(value = "SELECT count(*) + (select count(*) as test from forum.like_post where like_post.post_id = ?1) FROM forum.like_post as lp right join forum.post as p on lp.post_id=p.id \n" +
+            "right join forum.comment as c on p.id = c.belong_post_id \n" +
+            "right join forum.like_comment as lc on c.id = lc.comment_id \n" +
+            "where p.id = ?1",nativeQuery = true)
     int countPostAndCommentsLikes(long idPost);
 }
